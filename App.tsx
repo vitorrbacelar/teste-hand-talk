54e012c985aa2300411ff7aff90f5b7daf3181d4
vitorrbacelar <vitorrbacelar@gmail.com>
2025-01-10 23:56:24 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+function App(): React.JSX.Element {
+  return (
+    <View>
+      <Text>Teste hand talk</Text>
+    </View>
+  );
+}
+
+export default App;
